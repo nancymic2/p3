5fbaf9cb1fa469d20587fc41eea55c7da9b47f78
nancy michell <michell@fas.harvard.edu>
2014-10-12 13:30:19 -0400

    add pic

--- app/views/lorem.blade.php
+++ app/views/lorem.blade.php
@@ -1,5 +1,11 @@
 <?php 
 
+
+
+//$logo = File::get(app_path().'logo.jpg');
+
+echo '<img src="logo.jpg">';
+echo '<br>';
 $generator = new Badcow\LoremIpsum\Generator();
 $paragraphs = $generator->getParagraphs(5);
 echo implode('<p>', $paragraphs);
