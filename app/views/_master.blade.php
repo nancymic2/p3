a60e7bd9c8c5a6d1aa412145ac0fcdc5bf1f2566
nancy michell <michell@fas.harvard.edu>
2014-10-18 16:24:16 -0400

    add users

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -6,15 +6,16 @@
 
 </head>
 <body>
-<div style="width:830px; margin-left: 300px;">	
+<div style="font-family:'Arial',sans-Serif; width:800px; margin-left: 300px; padding-left: 10px; border: 1px solid gray;">	
+<div style="width:830px;">	
 <img src="logo.jpg">
 <br>
 <div style="width:795px;  margin-bottom: 10px; background-color: pink; height: 40px;">	
 <a href="/">Home</a> &nbsp;&nbsp;&nbsp; <a href="/">Text Gen</a>&nbsp;&nbsp;&nbsp; <a href="users">Fake Users</a>
 </div>
 
-@yield ('generator')
-<div style="width:730px; margin-top: 20px; border: 2px solid gray; padding: 20px;">	
+@yield ('generator') 
+<div style="width:730px; margin-top: 20px; margin-bottom: 20px; border: 1px solid gray; padding-left: 20px; padding-right: 20px;">	
 <h2>You can copy your text from here:</h2>
 @yield ('generator2')
 
@@ -24,6 +25,6 @@
 
 
 </div>
-
+</div>
 </body>
 </html>
